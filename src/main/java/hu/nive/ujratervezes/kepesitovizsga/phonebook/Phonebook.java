9cb3d3bdb3817f3f97a5a9668c4c1c9eb8736e40
obqo <ati.sz@freemail.hu>
2021-04-29 12:17:47 +0200

    PhoneBook-end

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/phonebook/Phonebook.java
@@ -1,5 +1,25 @@
 package hu.nive.ujratervezes.kepesitovizsga.phonebook;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Phonebook {
 
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException("Empty parameter!");
+        }
+        try (BufferedWriter writer = Files.newBufferedWriter(Path.of(output))) {
+            for (String key: contacts.keySet()) {
+                writer.write(key + ": " + contacts.get(key) + "\n");
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not write file!");
+        }
+    }
 }
